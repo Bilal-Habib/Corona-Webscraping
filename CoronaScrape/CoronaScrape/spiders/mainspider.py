b4c07da65ea6c248522844246ce9293f62fd01d9
55638034 <bilal.habib67@gmail.com>
2020-06-03 13:39:15 +0100

    table-commit

--- CoronaScrape/CoronaScrape/spiders/mainspider.py
+++ CoronaScrape/CoronaScrape/spiders/mainspider.py
@@ -8,15 +8,6 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        # for col in response.xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[2]/div/div[1]/table/thead//tr'):
-        #     yield  {
-        #         'head_1' : col.xpath('th[1]//text()').extract_first(),
-        #         'head_2' : col.xpath('th[2]//text()').extract_first(),
-        #         'head_3' : col.xpath('th[3]//text()').extract_first(),
-        #         'head_4' : col.xpath('th[4]//text()').extract_first(),
-        #         'head_5' : col.xpath('th[5]//text()').extract_first(),
-        #     }
-
         for row in response.xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[2]/div/div[1]/table/tbody//tr'):
             yield {
                 'Location' : row.xpath('th//text()').extract_first(),
