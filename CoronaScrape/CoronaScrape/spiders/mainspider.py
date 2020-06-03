4c154ed885c1c69c1f462dfacabba2ef78bce7c0
55638034 <bilal.habib67@gmail.com>
2020-06-03 13:33:04 +0100

    Commit where I have managed to scrape the main table

--- CoronaScrape/CoronaScrape/spiders/mainspider.py
+++ CoronaScrape/CoronaScrape/spiders/mainspider.py
@@ -8,14 +8,20 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        # self.cases_amount = response.xpath(
-        #     '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[1]/div[1]/div/div/div[1]/div[2]/text()').extract()
-        # self.deaths_amount = response.xpath(
-        #     '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[2]/div/div[1]/table/tbody/tr[2]/td[4]/text()').extract()
-        self.names = []
-        for column in range(3, 7):
-            for row in range(3, 13):
-                self.names[column][row] = response.xpath(
-                    '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[2]/div/div[1]/table/tbody/tr[%d]/td[%d]/text()' % (row, column)).extract()
+        # for col in response.xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[2]/div/div[1]/table/thead//tr'):
+        #     yield  {
+        #         'head_1' : col.xpath('th[1]//text()').extract_first(),
+        #         'head_2' : col.xpath('th[2]//text()').extract_first(),
+        #         'head_3' : col.xpath('th[3]//text()').extract_first(),
+        #         'head_4' : col.xpath('th[4]//text()').extract_first(),
+        #         'head_5' : col.xpath('th[5]//text()').extract_first(),
+        #     }
 
-    
+        for row in response.xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[2]/div[4]/div/div/div[2]/div/div[1]/table/tbody//tr'):
+            yield {
+                'Location' : row.xpath('th//text()').extract_first(),
+                'Confirmed' : row.xpath('td[1]//text()').extract_first(),
+                'Cases per 1 million people' : row.xpath('td[2]//text()').extract_first(),
+                'Recovered' : row.xpath('td[3]//text()').extract_first(),
+                'Deaths' : row.xpath('td[4]//text()').extract_first(),
+            }
