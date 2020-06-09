197af5d0e8b90305bd5c8658360d1d1be30164d9
55638034 <bilal.habib67@gmail.com>
2020-06-09 16:24:45 +0100

    Project Structure Change

---
+++ Webpage/Javascript/LoadTable.js
@@ -0,0 +1,17 @@
+// Loads Covid-19 Data
+
+d3.text("/Webpage/Tables/covid.csv", function (data) {
+    var parsedCSV = d3.csv.parseRows(data);
+
+    var container = d3.select("body")
+        .append("table")
+
+        .selectAll("tr")
+        .data(parsedCSV).enter()
+        .append("tr")
+
+        .selectAll("td")
+        .data(function (d) { return d; }).enter()
+        .append("td")
+        .text(function (d) { return d; });
+});
